621810596cc0cd1e18f2a37bf0a9cb6e54c0c38a
Alena Kodola <alenakodola@gmail.com>
2025-04-04 21:29:55 +0300

    add validate

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,23 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { nanoid } from "nanoid";
+import * as Yup from "yup";
 import css from "./ContactForm.module.css";
+
+const applySchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  number: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
 
 const ContactForm = ({ onAdd }) => {
   const handleSubmit = (values, actions) => {
     onAdd({
-      id: Date.now(),
+      id: nanoid(),
       name: values.name,
       number: values.number,
     });
@@ -18,15 +31,18 @@ const ContactForm = ({ onAdd }) => {
         number: "",
       }}
       onSubmit={handleSubmit}
+      validationSchema={applySchema}
     >
       <Form className={css.form}>
         <label className={css.label}>
           <span>Name</span>
           <Field className={css.field} type="text" name="name" />
+          <ErrorMessage className={css.error} name="name" component="span" />
         </label>
         <label className={css.label}>
           <span>Number</span>
           <Field className={css.field} type="tel" name="number" />
+          <ErrorMessage className={css.error} name="number" component="span" />
         </label>
         <button className={css.button} type="submit">
           Add contact
